faf0d79cf01a786d6008126973a41e04588e7beb
sslv <pignagnolisilvia@gmail.com>
2024-06-18 11:10:09 +0200

    Create Print_Even_Numbers.py

---
+++ Print_Even_Numbers.py
@@ -0,0 +1,9 @@
+start, stop=0, 10
+
+def print_even_numbers(start, stop):
+    
+    for n in range (start, stop):  
+        if n%2==0:
+            print(n)
+        
+print_even_numbers(start, stop)
