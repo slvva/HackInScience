42f3ef913ac6261edc183d33d52bf0af53c079ee
sslv <pignagnolisilvia@gmail.com>
2024-06-18 11:21:28 +0200

    Create PrintEveryPairDistinctLetters.py

---
+++ PrintEveryPairDistinctLetters.py
@@ -0,0 +1,18 @@
+import string
+
+x=y=0
+
+alphabet=string.ascii_lowercase
+
+for l in range(len(alphabet)*len(alphabet)-len(alphabet)):
+
+    if x==y:
+        y=y+1
+        
+    if y<len(alphabet)-1:
+            print(alphabet[x]+alphabet[y])
+            y=y+1
+    else:
+            print(alphabet[x]+alphabet[y])
+            y=0
+            x=x+1
