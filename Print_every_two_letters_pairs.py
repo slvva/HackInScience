f9b6750787c24ee972841b07ef675ba3cd3d3682
sslv <pignagnolisilvia@gmail.com>
2024-06-18 11:18:30 +0200

    Create Print_every_two_letters_pairs.py

---
+++ Print_every_two_letters_pairs.py
@@ -0,0 +1,15 @@
+import string
+
+alphabet=string.ascii_lowercase
+
+x=y=0
+
+for l in range(len(alphabet)**2):
+ 
+    if y<len(alphabet)-1:
+        print(alphabet[x]+alphabet[y])
+        y=y+1
+    else:
+        print(alphabet[x]+alphabet[y])
+        y=0
+        x=x+1
