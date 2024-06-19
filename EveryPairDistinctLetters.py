0c7e07352b9205a3ac9dcbefd931abb57c0a676a
sslv <pignagnolisilvia@gmail.com>
2024-06-19 10:39:50 +0200

    EveryPairDistinctLetters.py

--- EveryPairDistinctLetters.py
+++ EveryPairDistinctLetters.py
@@ -4,7 +4,7 @@ x=y=0
 
 alphabet=string.ascii_lowercase
 
-for l in range(len(alphabet)*len(alphabet)-len(alphabet)):
+for _ in range(len(alphabet)*len(alphabet)-len(alphabet)):
 
     if x==y:
         y=y+1
